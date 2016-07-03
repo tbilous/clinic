45b49cd83c7e3707233680400f3437b727bffcd7
Taras Bilous <tbilous@yahoo.com>
2016-07-03 12:22:46 +0000

    Mailer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
-  def title(page_title)
-    content_for :title, page_title.to_s
+  # def title(page_title)
+  #   content_for :title, page_title.to_s
+  # end
+  def full_title(page_title)
+    base_title = t(:site_name)
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} + ' ' + t(:site_name)"
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'clinic-stas.herokuapp.com' }
+  
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }
 end
