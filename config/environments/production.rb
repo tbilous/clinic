4fbc9e73d0f002c30e01f500bb3ff69e9bc2ed51
Taras Bilous <tbilous@yahoo.com>
2016-07-15 09:54:03 +0000

    Issue?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   #   authentication: "plain",
   #   enable_starttls_auto: true,
   #   user_name: ENV["GMAIL_USERNAME"],
-  #   password: ENV["GMAIL_PASSWORD"]
+  #   password: ENV["GMAIL_PASSWORD"] 
   # }
 
   # Devise includes its ow
