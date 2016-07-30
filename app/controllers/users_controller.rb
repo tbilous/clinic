eeb1e7500dfbb39df91ef903f3f2afebc2600d9a
Taras Bilous <tbilous@yahoo.com>
2016-07-30 20:25:57 +0300

    Change flash, controll access to index and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   # before_action :signed_in_user, only: [:edit, :update, :update, :destroy]
   # before_action :correct_user,   only: [:edit, :update]
   # before_action :admin_user,     only: :destroy
-  before_action :authenticate_user!, only: [:edit, :show, :index, :update, :destroy]
+  before_action :authenticate_user!, only: [ :edit, :show, :update ]
+  before_action :current_user_admin, only: [ :edit, :show, :update, :index, :destroy]
+
   require 'will_paginate'
   def new
     @user = User.new
@@ -11,38 +13,46 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def index
-
-  # @user = User.find_by_name(params[:id])
-
-    # User.paginate(page: params[:page])
-    @users = User.paginate(:page => params[:page], :per_page => 15).order('created_at DESC')
-    # @users = User.all
+    if current_user_admin
+      @users = User.paginate(:page => params[:page], :per_page => 15).order('created_at DESC')
+    else
+      redirect_to root_path
+    end
   end
-  
+
   def edit
      @user = User.find(params[:id])
   end
-  
+
   def update
   end
-  
+
   def create
-    
+
   end
-  
+
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
+    if User.find(params[:id]) == current_user
+      redirect_to users_url
+      flash[:error] = t('edit.delete_active')
+    else
+      User.find(params[:id]).destroy
+      flash[:success] = t('edit.delete_succes')
+      redirect_to users_url
+    end
   end
-  
-  
+
+
   private
 
     # def user_params
     #   params.require(:user).permit(:email, :password, :password_confirmation)
     # end
+    def current_user_admin
+      current_user.admin?
+    end
+
 
 end
--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,14 +1,35 @@
+# def devise_error_messages!
+#    return "" unless devise_error_messages?
+#
+#    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+#    sentence = I18n.t("errors.messages.not_saved",
+#                      :count => resource.errors.count,
+#                      :resource => resource.class.model_name.human.downcase)
+#
+#    html = <<-HTML
+#    <div id="error_explanation">
+#      <h2>#{sentence}</h2>
+#      <ul>#{messages}</ul>
+#    </div>
+#    HTML
+#
+#    html.html_safe
+#  end
+#
+#  def devise_error_messages?
+#    !resource.errors.empty?
+#  end
+
 module DeviseHelper
  def devise_error_messages!
   return '' if resource.errors.empty?
-
    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
    sentence = I18n.t('errors.messages.not_saved',
    count: resource.errors.count,
    resource: resource.class.model_name.human.downcase)
 
    html = <<-HTML
-   <div class="alert alert-danger danger-block alert-text" role="alert"> 
+   <div class="alert alert-danger danger-block alert-text" role="alert">
    <button type="button" class="close" data-dismiss="alert">
     <span class="glyphicon glyphicon-remove-circle"></span>
    </button>
@@ -20,5 +41,9 @@ module DeviseHelper
    HTML
 
    html.html_safe
+ end
+ def devise_error_messages?
+  #  !resource.errors.empty?
+   flash_messages
  end
 end
