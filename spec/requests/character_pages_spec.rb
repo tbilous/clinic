02541c34f6902ade8781db442878689a0b7584c4
Taras Bilous <tbilous@yahoo.com>
2016-08-18 14:37:38 +0000

    remove unused

--- spec/requests/character_pages_spec.rb
+++ spec/requests/character_pages_spec.rb
@@ -1,15 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "CharacterPages", type: :view do
-  Devise::Test::ControllerHelpers
   subject { page }
-  # before :each do
-  #     @request.env["devise.mapping"] = Devise.mappings[:admin]
-  #     @request.env["devise.mapping"] = Devise.mappings[:user]
-  #     @admin = FactoryGirl.create :admin
-  #     @user = FactoryGirl.create :user
-  #     sign_in @admin
-  # end
   include Devise::Test::ControllerHelpers 
   before :each do
       @request.env["devise.mapping"] = Devise.mappings[:admin]
@@ -18,42 +10,6 @@ RSpec.describe "CharacterPages", type: :view do
       @user = FactoryGirl.create :user
       # sign_in @admin
   end
-
-  # describe "micropost creation" do
-  #   before { visit root_path }
-
-  #   describe "with invalid information" do
-
-  #     it "should not create a micropost" do
-  #       expect { click_button "Post" }.not_to change(Micropost, :count)
-  #     end
-
-  #     describe "error messages" do
-  #       before { click_button "Post" }
-  #       it { should have_content('error') }
-  #     end
-  #   end
-
-  #   describe "with valid information" do
-
-  #     before { fill_in 'micropost_content', with: "Lorem ipsum" }
-  #     it "should create a micropost" do
-  #       expect { click_button "Post" }.to change(Micropost, :count).by(1)
-  #     end
-  #   end
-    
-  #   describe "micropost destruction" do
-  #     before { FactoryGirl.create(:micropost, user: user) }
-  
-  #     describe "as correct user" do
-  #       before { visit root_path }
-  
-  #       it "should delete a micropost" do
-  #         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
-  #       end
-  #     end
-  #   end
-  # end
 
   describe "in the Character pages without characters" do
     before { login(@user) }
@@ -73,7 +29,6 @@ RSpec.describe "CharacterPages", type: :view do
           find("#character_birthday_2i").select(Date::MONTHNAMES[character.birthday.month])
           find("#character_birthday_3i").select(character.birthday.day.to_s)
       	  find('#character_sex_true').set(true)
-          # find('.btn-sent').click
         end
         it {  expect { (find('.btn-sent').click) }.to change(Character, :count).by(1) }
       	describe "check result" do 
@@ -98,14 +53,12 @@ RSpec.describe "CharacterPages", type: :view do
       expect(page).to have_link( '', href: edit_character_path(character) ) 
     end
     describe "edit character" do
-      # it { puts page.body }
       before {  click_link('', href: edit_character_path(character)) }
       it { expect(page).to have_content character.name }
       it { expect(page).to have_title t('page.character.edit')  }
     end
   end
   describe "Edit character" do
-    # let!(:user) { FactoryGirl.create(:user) }
     let!(:character){ FactoryGirl.create(:character, user: @user) } 
     before do 
       login(@user)
