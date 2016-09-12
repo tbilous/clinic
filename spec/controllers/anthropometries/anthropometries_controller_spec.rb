295c105fd00e68130573e340fae5768892f1a510
Taras Bilous <tbilous@yahoo.com>
2016-09-12 23:13:31 +0300

    anthropometry create - test error

--- spec/controllers/anthropometries/anthropometries_controller_spec.rb
+++ spec/controllers/anthropometries/anthropometries_controller_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe AnthropometriesController, type: :controller do
       let!(:adata) { FactoryGirl.create(:adata, user_id: @character.user_id, character: @character) }
       # binding.pry
       before { delete :destroy, id: adata.id }
-      specify { expect(response).to redirect_to(root_path) }
+      specify { expect(response).to character_path(root_path) }
     end
   end
 
@@ -43,7 +43,6 @@ RSpec.describe AnthropometriesController, type: :controller do
       sign_in @admin
     end
     describe 'and owner users' do
-
       describe 'DELETE destroy' do
         let!(:anthropometry) { FactoryGirl.create(:adata, user_id: @admin.id, character: @character) }
         it { expect { delete :destroy, id: anthropometry.id }.to change { Anthropometry.count }.by(-1) }
